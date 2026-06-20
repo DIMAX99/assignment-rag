b2e31530c59db2f70fe764ce700a53e159347a42
DIMAX99 <divitc2004@gmail.com>
2026-06-20 19:30:47 +0530

    commented docling and huggingface

--- backend/model/embedding_model.py
+++ backend/model/embedding_model.py
@@ -2,12 +2,12 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import os
 def get_google_embedding():
     return GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY"))
-from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_huggingface import HuggingFaceEmbeddings
 
-def get_embedding_model_hf():
-    return HuggingFaceEmbeddings(
-        model_name="BAAI/bge-small-en-v1.5",
-        model_kwargs={"device": "cpu"},
-        encode_kwargs={"normalize_embeddings": True},
-    )
+# def get_embedding_model_hf():
+#     return HuggingFaceEmbeddings(
+#         model_name="BAAI/bge-small-en-v1.5",
+#         model_kwargs={"device": "cpu"},
+#         encode_kwargs={"normalize_embeddings": True},
+#     )
 
--- backend/parser/parser.py
+++ backend/parser/parser.py
@@ -26,9 +26,9 @@
 # ocr_pdf_parser("data/bio.pdf")
 
 
-from docling.document_converter import DocumentConverter
+# from docling.document_converter import DocumentConverter
 
-source = "data/bio.pdf"  # document per local path or URL
-converter = DocumentConverter()
-result = converter.convert(source)
-print(result.document.export_to_markdown())  # output: "## Docling Technical Report[...]"
+# source = "data/bio.pdf"  # document per local path or URL
+# converter = DocumentConverter()
+# result = converter.convert(source)
+# print(result.document.export_to_markdown())  # output: "## Docling Technical Report[...]"
